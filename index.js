431d60229cf0214cbd18d4b9271bbabdee571397
Gavin Moore <gavizoid16@gmail.com>
2018-09-13 20:14:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function takeANumber(katzDeli, name) {
     katzDeli.push(name)
   return ("Welcome, "+ name +". You are number "+(katzDeli.length)+" in line.")
 }
+
+
+
 function nowServing(katzDeliLine) {
-  let i = 0
- if (i < katzDeliLine.length) {
+ if (0 < katzDeliLine.length) {
    return ("Currently serving "+(katzDeliLine.shift(name))+".")
  } else {
    return ("There is nobody waiting to be served!")
